f65d9b03c4bbe8ee82a98757cb4f719ea00df121
Matteo Cappella <matteo.cappella@leitha.eu>
2022-03-28 12:44:12 +0200

    converter uploaded

---
+++ main.py
@@ -0,0 +1,17 @@
+import csv, json 
+
+def csv_to_json(csvFilePath, jsonFilePath):
+    jsonArray = []
+      
+    with open(csvFilePath, encoding='utf-8') as csvf: 
+        csvReader = csv.DictReader(csvf, delimiter=";") 
+        for row in csvReader: 
+            jsonArray.append(row)
+  
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+        jsonString = json.dumps(jsonArray, indent=4)
+        jsonf.write(jsonString)
+          
+csvFilePath = r'multireport.csv'
+jsonFilePath = r'multireport.json'
+csv_to_json(csvFilePath, jsonFilePath)
